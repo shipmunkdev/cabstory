08a6cccb0b6388678c01c3609e97476c992635cb
Sein Tun <contact@seintun.dev>
2023-05-01 01:13:43 -0700

    chore(vercel/analytics): added @vercel/analytics and removed yarn lint for vercel deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'CabStory',
@@ -8,7 +9,10 @@ export const metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 };
---
+++ docs/CHANGELOG.md
@@ -0,0 +1,5 @@
+# 1.0.0 (2023-05-01)
+
+### Bug Fixes
+
+- **package.json:** remove yarn lint during build process. vercel build exitr ([475e8d4](https://github.com/shipmunkdev/cabstory/commit/475e8d4ea874f5eda6eac9fe0797e0ec75fbe8a1))
